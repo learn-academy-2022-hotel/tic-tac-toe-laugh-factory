ade1e676290d30050ef3afbd1e9c8dade249fbe4
lmhazel <leahazel2000@gmail.com>
2023-01-03 13:58:42 -0800

    user stories 1 and 2

--- src/App.js
+++ src/App.js
@@ -3,14 +3,36 @@ import Square from './components/Square'
 import './App.css'
 
 const App = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null))
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [player, setPlayer] = useState("player 1")
+
+  const handleGamePlay = (clickSquare) => {
+    let updateBoard = [...board]
+    updateBoard[clickSquare]="X"
+    setBoard(updateBoard)
+  }
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+  }
 
   return (
     <>
       <h1>Tic Tac Toe</h1>
-      <Square />
+      <div className="board">
+      {board.map((shape, index) => {
+        return(
+        <Square 
+        shape={shape}
+        index={index}
+        handleGamePlay={handleGamePlay}
+        />
+        )
+        })}
+        <button onClick={resetGame}>Restart</button>
+      </div>
     </>
   )
+
 }
 
 export default App
