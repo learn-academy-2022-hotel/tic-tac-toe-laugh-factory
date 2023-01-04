3f2b75822dcd52d05e0ca04c0200370635c4a7e7
lmhazel <leahazel2000@gmail.com>
2023-01-03 16:33:17 -0800

    show who wins and have only one mark her square

--- src/App.js
+++ src/App.js
@@ -4,35 +4,66 @@ import './App.css'
 
 const App = () => {
   const [board, setBoard] = useState(Array(9).fill(null))
-  const [player, setPlayer] = useState("player 1")
+  const [player1, setPlayer1] = useState(true)
 
-  const handleGamePlay = (clickSquare) => {
-    let updateBoard = [...board]
-    updateBoard[clickSquare]="X"
-    setBoard(updateBoard)
+  
+  const handleGamePlay = (index) => {
+    //if(isaWinner || updateBoard[index]) return;
+    if(board[index] === null) {
+      let updateBoard = [...board]
+      updateBoard[index]= player1 ? "X" : "O"
+      setBoard(updateBoard)
+      setPlayer1(!player1)
+    }
   }
+  
+  const isaWinner = (squares) => {
+    let lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+  }
+
+  const winner = isaWinner(board)
+  
   const resetGame = () => {
     setBoard(Array(9).fill(null))
   }
-
+  
   return (
     <>
       <h1>Tic Tac Toe</h1>
+      {winner &&
+      <h2>{winner} is the winner</h2>}
       <div className="board">
       {board.map((shape, index) => {
         return(
-        <Square 
-        shape={shape}
-        index={index}
-        handleGamePlay={handleGamePlay}
-        />
-        )
+          <Square 
+          shape={shape}
+          index={index}
+          handleGamePlay={handleGamePlay}
+          />
+          )
         })}
         <button onClick={resetGame}>Restart</button>
       </div>
     </>
   )
-
+  
 }
+
 
 export default App
